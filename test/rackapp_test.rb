7b0e94b1606509e0f580840608b94a4b19e9f91d
b-artem <artemb.dev@gmail.com>
2017-07-02 21:01:34 +0300

    add endpoint tests

---
+++ test/rackapp_test.rb
@@ -0,0 +1,42 @@
+require "test/unit"
+require "rack/test"
+require './lib/rackapp'
+require 'erb'
+
+class RackAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    app = RackApp
+    builder = Rack::Builder.new
+    builder.run app
+  end
+
+  def test_root
+    get '/'
+    assert last_response.ok?
+  end
+
+  def test_start
+    get '/start'
+    follow_redirect!
+    assert last_response.ok?
+  end
+
+  def test_submit_guess
+    get '/submit_guess'
+    follow_redirect!
+    assert last_response.ok?
+  end
+
+  def test_hint
+    get '/hint'
+    follow_redirect!
+    assert last_response.ok?
+  end
+
+  def test_not_found
+    get '/something_not_existing'
+    assert_equal last_response.status, 404
+  end
+end
